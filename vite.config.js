471f1665df2dd9b7f919fa7ed5ee088bc84457a2
Elvis <elviswang@podoteng.com>
2023-06-14 19:12:28 +0800

    修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { VantResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    base: '/vue-study/',
+    base: '/vue-study',
     port: 8080
   },
   plugins: [
@@ -18,5 +18,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  publicDir: "public",
+  base: "./"
+
 })
