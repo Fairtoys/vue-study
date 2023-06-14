a8760104984e2c4580fb6d32f907595c923f668e
Elvis <elviswang@podoteng.com>
2023-06-14 16:58:55 +0800

    添加/

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { VantResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    base: 'vue-study',
+    base: '/vue-study/',
     port: 8080
   },
   plugins: [
